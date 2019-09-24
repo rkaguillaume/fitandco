f27a8b6a0265e0088454c78acee7d3f7e54c4360
Guillaume Roche <guillaumeroche24@gmail.com>
2019-09-24 10:12:51 +0200

    front fashboard home

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,5 +1,8 @@
 class DashboardsController < ApplicationController
   layout 'nonavbar'
   def index
+    @coachs = Coach.all.size
+    @courses = Course.all.size
+    @plannings = Planning.all.size
   end
 end
