acfba58ca49f01f857fd1f5ffb23276325ecb509
Guillaume Roche <guillaumeroche24@gmail.com>
2019-10-03 13:14:56 +0200

    404 & 500 pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  match "/404", :to => "errors#not_found", :via => :all
+  match "/500", :to => "errors#internal_server_error", :via => :all
+
   devise_for :users, controllers: {
         sessions: 'users/sessions',
         registrations: 'users/registrations'
-      }
+    }
 
   root to: 'pages#home'
   get 'les-cours', to: 'pages#courses'
